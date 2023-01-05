1591973763f7dbc0b2c4a259b736c147e969d280
rolix202 <williamsroland147@gmail.com>
2023-01-06 00:29:43 +0100

    Write a program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+
+    arguments = [int(args) for args in arguments]
+
+    result = sum(arguments)
+    print("Result starts here")
+    print(result)
